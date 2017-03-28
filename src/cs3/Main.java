2fbe475d3ed83c05b102f5bfd2eead50e95a988b
catpilo <catpilo@users.noreply.github.com>
2017-03-29 01:47:26 +0200

    Added test cases in the main class for CS3

--- src/cs3/Main.java
+++ src/cs3/Main.java
@@ -2,12 +2,18 @@ package cs3;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		BrickTest bt = new BrickTest(32);
-		System.out.println(bt.test(100, 20));
-	}
 
+	public static void main(String[] args) {
+		BrickTest bt = new BrickTest(9);
+		int[] k_val = {1, 2, 10, 100, 1000};
+		for(int n = 10; n <= 1000000; n *= 10) {
+			for(int k = 0; k < k_val.length; k++) {
+				long t_start = System.nanoTime();
+				bt.test(n,  k_val[k]);
+				long t_elapsed = System.nanoTime() - t_start;
+				System.out.format("n=%d, k=%d, tiradas=%d, " +
+						"tiempo=%d%n", n, k_val[k], bt.getTiradas(), t_elapsed);
+			}
+		}
+	}
 }
