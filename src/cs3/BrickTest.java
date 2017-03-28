9899db7ca6bcab9ca29dee6eb048c550e55d0ff2
catpilo <catpilo@users.noreply.github.com>
2017-03-29 01:46:49 +0200

    Fixed BrickTest's divide and conquer algorithm
    A counter "tiradas" was implemented to count the number of tries.

--- src/cs3/BrickTest.java
+++ src/cs3/BrickTest.java
@@ -2,30 +2,59 @@ package cs3;
 
 public class BrickTest {
 	private int breaksAt;
-	
+	private int tiradas;
+
 	public BrickTest(int breaksAt) {
 		this.breaksAt = breaksAt;
+		this.tiradas = 0;
+	}
+	
+	
+	public int getTiradas() {
+		return tiradas;
+	}
+
+	public void setTiradas(int tiradas) {
+		this.tiradas = tiradas;
 	}
 	
 	public int test(int n, int k) {
+		tiradas = 0;
 		return test(1, n, k);
 	}
 	
+	
+	public int getBreaksAt() {
+		return breaksAt;
+	}
+
+
+	public void setBreaksAt(int breaksAt) {
+		this.breaksAt = breaksAt;
+	}
+
+
 	private int test(int beg, int end, int k) {
-		System.out.println("Test llamado con beg = " + beg + " end = " + end);
 		int breaks = -1;
-		if(k == 1) {
+		if (beg == end) {
+			breaks = beg;
+		} else if(k == 1) {
 			for(int i=beg; i <= end; i++) {
+				tiradas++;
 				if (i == breaksAt) {
 					breaks = i;
+					break;
 				}
-			}			
+			}	
 		} else if(k >= 2) {
-			breaks = test(beg, (end-beg)/2, k-1);
-			if (breaks == -1) {
-				breaks = test((end-beg)/2, end, k-1);
+			int mitad = (end-beg)/2+beg;
+			tiradas++;
+			if(breaksAt <= mitad) { // se rompe
+				breaks = test(beg, mitad, k-1);
+			} else { // no se rompe
+				breaks = test(mitad+1, end, k-1);
 			}
-		}		
+		}	
 		return breaks;
 	}
 	
